3e980e89581039393037979daa929b44bfd9ee37
Zver64 <coolvill@mail.ru>
2020-03-11 13:57:22 +0300

    added fonts and base styles

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,20 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
 html {
   box-sizing: border-box;
-  background-color: $c-background;
+  background-color: var(--grey);
+  font-family: 'Montserrat', sans-serif;
+  width: 100%;
+}
+* {
+  box-sizing: inherit;
 }
 #app {
-  max-width: 1200px;
+  max-width: 100vw;
+  width: 1200px;
+  padding: 0 15px;
   margin: 0 auto;
 }
 </style>
